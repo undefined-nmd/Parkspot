5e4cd089e900e51171b282428d34a7a9791d09b9
Jan Temmerman <jantemme@student.arteveldehs.be>
2019-11-18 15:45:05 +0100

    new drawer works, apple style

--- views/HomeView.tsx
+++ views/HomeView.tsx
@@ -20,7 +20,7 @@ export default function HomeView() {
 
     const handleTextSubmit = (event: TextInputSubmitEditingEventData) => {
         console.log(event.text)
-        openControlPanel()
+        closeControlPanel()
     }
 
     useEffect(() => { 
@@ -32,7 +32,7 @@ export default function HomeView() {
         panThreshold={0.1}
         type={'overlay'}
         tweenDuration={240}
-        openDrawerOffset={0.2}
+        openDrawerOffset={0.1}
         closedDrawerOffset={0.1}
         negotiatePan={true}
         side={'bottom'}
@@ -43,32 +43,17 @@ export default function HomeView() {
                 style={{
                     width: 40,
                     height: 6,
-                    margin: 4,
+                    margin: 8,
                     marginTop: 8,
                     borderRadius: 10,
                     backgroundColor: '#cfcfcf',
                     alignSelf: 'center',
                 }}
                 />
-                <View style={{flex:1, width: '93%', alignSelf:'center', padding: 6}}>
-                    <Text style={{fontSize: 26, fontWeight: '700', color: '#3f3f3f'}}>Favorieten</Text>
-                </View>
-            </View>
-        }
-        >
-            <View
-            style={{ flex:1 }}
-            >
-                <StatusBar barStyle="dark-content"/>
-                <MapView
-                    style={{flex: 1,
-                        width: '100%',
-                        height: '100%',}}
-                />
-                <KeyboardAvoidingView style={{width: '100%', padding: 6, bottom: 30, justifyContent: 'center', alignItems: 'center', position: "absolute",alignSelf: 'center', backgroundColor: 'white', elevation: 2}} behavior="padding" enabled keyboardVerticalOffset={6}>
                 <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'center', borderRadius: 12, backgroundColor: '#ebebeb', width: '93%', alignSelf: 'center'}}>
                         <Image style={{width: 22, height: 22}} source={require('../assets/icons8-search.png')}/>
                         <TextInput
+                        onFocus={openControlPanel}
                         returnKeyType={'search'}
                         onSubmitEditing={(event) => handleTextSubmit(event.nativeEvent)}
                         autoCompleteType={"street-address"}
@@ -89,30 +74,21 @@ export default function HomeView() {
                         value={value}
                         />
                     </View>
-                </KeyboardAvoidingView>
-                <View style={{width: '100%', backgroundColor: 'white', bottom: 0, height: 30, zIndex: 1}}></View>
-                {/*<BottomDrawer
-                containerHeight={400}
-                downDisplay={300}
-                backgroundColor={"#FFF"}
-                shadow={false}
-                startUp={false}
-                >
-                    <View
-                    style={{
-                        width: 40,
-                        height: 6,
-                        margin: 4,
-                        marginTop: 8,
-                        borderRadius: 10,
-                        backgroundColor: '#cfcfcf',
-                        alignSelf: 'center',
-                    }}
-                    />
-                    <View style={{flex:1, width: '93%', alignSelf:'center', padding: 6}}>
-                        <Text style={{fontSize: 26, fontWeight: '700', color: '#3f3f3f'}}>Favorieten</Text>
-                    </View>
-                </BottomDrawer>*/}
+                <View style={{flex:1, width: '93%', alignSelf:'center', padding: 6, marginTop: 20}}>
+                    <Text style={{fontSize: 26, fontWeight: '700', color: '#3f3f3f'}}>Preferences</Text>
+                </View>
+            </View>
+        }
+        >
+            <View
+            style={{ flex:1 }}
+            >
+                <StatusBar barStyle="dark-content"/>
+                <MapView
+                    style={{flex: 1,
+                        width: '100%',
+                        height: '100%',}}
+                />
             </View>
         </Drawer>
     );
